57e6973e87674e190946c29b575523cb2b9841b8
MsUliana <ulianadzoba@gmail.com>
2020-01-27 10:33:34 +0200

    Updates

---
+++ precache-manifest.ec130ad62f771b2827cbb7f16bb5fc80.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6eb5c6a7144ec3242c5e1189e8c74e",
+    "url": "/bestbuter_project/index.html"
+  },
+  {
+    "revision": "cddf5938d9be79b67c6c",
+    "url": "/bestbuter_project/static/css/2.ead3faf1.chunk.css"
+  },
+  {
+    "revision": "1cb8a6d65806ed8fa4ea",
+    "url": "/bestbuter_project/static/css/main.8f72b265.chunk.css"
+  },
+  {
+    "revision": "cddf5938d9be79b67c6c",
+    "url": "/bestbuter_project/static/js/2.29522f4c.chunk.js"
+  },
+  {
+    "revision": "77ccaffd16cd0eb4657e5506cc1cbbf7",
+    "url": "/bestbuter_project/static/js/2.29522f4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cb8a6d65806ed8fa4ea",
+    "url": "/bestbuter_project/static/js/main.1c0163c2.chunk.js"
+  },
+  {
+    "revision": "b3cf19adc29c3350586d",
+    "url": "/bestbuter_project/static/js/runtime-main.9924153c.js"
+  },
+  {
+    "revision": "80070ecad276273efd036ab92ef3dfd6",
+    "url": "/bestbuter_project/static/media/1.80070eca.JPG"
+  },
+  {
+    "revision": "d790a5c3ae65cb419cf572d6e2400da8",
+    "url": "/bestbuter_project/static/media/11.d790a5c3.jpg"
+  },
+  {
+    "revision": "5cb53951bbfa3298dac3741133ab2452",
+    "url": "/bestbuter_project/static/media/2.5cb53951.JPG"
+  },
+  {
+    "revision": "cc9ef2edbe59f38bfe1d6067c32542f6",
+    "url": "/bestbuter_project/static/media/22.cc9ef2ed.jpg"
+  },
+  {
+    "revision": "10faf0383a0983513d214f56151a9992",
+    "url": "/bestbuter_project/static/media/3.10faf038.jpg"
+  },
+  {
+    "revision": "b1203c3f6aab665700f590c130a0a1ed",
+    "url": "/bestbuter_project/static/media/33.b1203c3f.jpg"
+  },
+  {
+    "revision": "6fbbaf160121f08890398441e56f5e6f",
+    "url": "/bestbuter_project/static/media/Amsdam-Regular.6fbbaf16.woff"
+  },
+  {
+    "revision": "9ffece672ab0c6b4cab30a96e88b875f",
+    "url": "/bestbuter_project/static/media/Amsdam-Regular.9ffece67.ttf"
+  },
+  {
+    "revision": "5f5750712d0f40339fb744b431edd51c",
+    "url": "/bestbuter_project/static/media/Chef.5f575071.jpg"
+  },
+  {
+    "revision": "0627c607292f3c77d5b17cf7a849f752",
+    "url": "/bestbuter_project/static/media/about.0627c607.jpg"
+  },
+  {
+    "revision": "c06cd8e7ad216f2a48215cb859973119",
+    "url": "/bestbuter_project/static/media/footer_bg.c06cd8e7.jpg"
+  },
+  {
+    "revision": "40ba1e201cd483768cb0ebfcd667539d",
+    "url": "/bestbuter_project/static/media/head_bg1.40ba1e20.jpg"
+  },
+  {
+    "revision": "eafab6fdbaa2bbb545086fabfa8ce60c",
+    "url": "/bestbuter_project/static/media/head_bg2.eafab6fd.jpg"
+  },
+  {
+    "revision": "977c645528cb4cff5885679ff47832f1",
+    "url": "/bestbuter_project/static/media/head_bg3.977c6455.jpg"
+  },
+  {
+    "revision": "43cd7fc26baf22746060887a376edf12",
+    "url": "/bestbuter_project/static/media/img1.43cd7fc2.jpg"
+  },
+  {
+    "revision": "1eee5b12941774b35c6e6b8a3cf4a01b",
+    "url": "/bestbuter_project/static/media/img2.1eee5b12.jpg"
+  },
+  {
+    "revision": "652eb68281f774244aa7488a5b31990a",
+    "url": "/bestbuter_project/static/media/img3.652eb682.jpg"
+  },
+  {
+    "revision": "d96b47006b21b8129b135a9c217b7e8a",
+    "url": "/bestbuter_project/static/media/img4.d96b4700.jpg"
+  },
+  {
+    "revision": "6756d872d37eecca18b774307ca48a51",
+    "url": "/bestbuter_project/static/media/img5.6756d872.jpg"
+  },
+  {
+    "revision": "b15819c1d57a105f9cde7807aada43aa",
+    "url": "/bestbuter_project/static/media/img6.b15819c1.jpg"
+  },
+  {
+    "revision": "56430fb74a8d88b795e12af4469114da",
+    "url": "/bestbuter_project/static/media/img7.56430fb7.jpg"
+  },
+  {
+    "revision": "27ace30b0420a135dc59d7a9defce015",
+    "url": "/bestbuter_project/static/media/img8.27ace30b.jpg"
+  },
+  {
+    "revision": "25aef4d3106eead54dc02e3688337a43",
+    "url": "/bestbuter_project/static/media/owner_bg.25aef4d3.jpg"
+  },
+  {
+    "revision": "f7772e51fab7a39b1e3aef0c8057b0d7",
+    "url": "/bestbuter_project/static/media/services_bg.f7772e51.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bestbuter_project/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bestbuter_project/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bestbuter_project/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bestbuter_project/static/media/slick.f97e3bbf.svg"
+  }
+]);
