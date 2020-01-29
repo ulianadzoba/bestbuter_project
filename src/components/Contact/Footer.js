a40ae2a3a12ed8ace62c365894a781c64da4ac53
MsUliana <ulianadzoba@gmail.com>
2020-01-29 08:49:11 +0200

    Add files before build

--- src/components/Contact/Footer.js
+++ src/components/Contact/Footer.js
@@ -4,32 +4,80 @@ import SocialLinks from '../SocialLinks/SocialLinks';
 import './Footer.scss';
 import ClientContext from '../ClientContext/ClientContext';
 
+const checkEmail = RegExp(`^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$`);
+
+const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+        (val) => val.length > 0 && (valid = false)
+    );
+    return valid;
+}
 class Footer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: "",
-            email: "",
-            phone: "",
+            name: null,
+            email: null,
+            phone: null,
             web: "",
-            message: ""
+            message: "",
+            openForm: false,
+            errors: {
+                name: '',
+                email: '',
+                phone: ''
+            }
         };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        const target = event.target;
-        let value = target.value;
-        let name = target.name;
-        this.setState({ [name]: value });
+    handleChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        let errors = this.state.errors;
+
+        switch (name) {
+            case 'name':
+                errors.name =
+                    (value.length < 5)
+                        ? 'Your name must be at least 5 characters'
+                        : '';
+                break;
+            case 'email':
+                errors.email =
+                    checkEmail.test(value)
+                        ? ''
+                        : 'Email is not valid';
+                break;
+            case 'phone':
+                errors.phone =
+                    (value.length < 9)
+                        ? 'Phone must be 9 characters long'
+                        : '';
+                break;
+            default:
+                break;
+        }
+        this.setState({ errors, [name]: value });
     }
 
-    handleSubmit(event) {
-        console.log(this.state.name);
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (validateForm(this.state.errors)) {
+            this.setState({
+                openForm: !this.state.openForm
+            });
+        } else {
+            this.setState({
+                openForm: this.state.openForm
+            });
+        }
+
         event.preventDefault();
     }
+
     render() {
+        const { errors } = this.state;
         return (
             <section className="footer" id="footer">
                 <div className="container">
@@ -54,57 +102,61 @@ class Footer extends Component {
                             </div>
                             <div className="phone">+380663338888</div>
                         </div>
-                        <div className="col-xl-8 col-lg-8 footer-form">
-                            <form action="" onSubmit={this.handleSubmit}>
+                        <div className={`col-xl-8 col-lg-8 footer-form ${this.state.openForm ? "close-form" : ""} `}>
+                            <form action="" onSubmit={event => this.handleSubmit(event)}>
                                 <div className="row name-email ">
                                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 input-name">
                                         <input className="input"
                                             name="name"
                                             type="text" placeholder="Name*"
-                                            value={this.state.name}
                                             onChange={this.handleChange}
                                         />
+                                        {errors.name.length > 0 &&
+                                            <div className='error'>{errors.name}</div>}
                                     </div>
                                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
                                         <input className="input"
                                             name="email"
                                             type="email"
                                             placeholder="E-mail"
-                                            value={this.state.email}
                                             onChange={this.handleChange}
                                         />
+                                        {errors.email.length > 0 &&
+                                            <div className='error'>{errors.email}</div>}
                                     </div>
                                 </div>
                                 <div className="row phones ">
                                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 input-phone">
                                         <input className="input"
                                             type="text"
+                                            name="phone"
                                             placeholder="Phone*"
-                                            value={this.state.value}
                                             onChange={this.handleChange}
                                         />
+                                        {errors.phone.length > 0 &&
+                                            <div className='error'>{errors.phone}</div>}
                                     </div>
                                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 ">
                                         <input className="input"
                                             type="text"
                                             placeholder="Adress"
-                                            value={this.state.value}
+
                                             onChange={this.handleChange}
                                         />
+
                                     </div>
                                 </div>
                                 <div className="row message ">
                                     <div className="col-xl-12 col-lg-12 col-md-12 ">
-                                        <ClientContext.Consumer>
-                                            {(context) => (
-                                                <input className="input"
-                                                    type="text"
-                                                    placeholder="Message*"
-                                                    value={context.state.text}
-                                                    onChange={this.handleChange} />
-                                            )}
+                                        {/* <ClientContext.Consumer>
+                                                {(context) => ( */}
+                                        <input className="input"
+                                            type="text"
+                                            placeholder="Message*"
+                                            onChange={this.handleChange} />
+                                        {/* )} */}
 
-                                        </ClientContext.Consumer>
+                                        {/* </ClientContext.Consumer> */}
                                     </div>
                                 </div>
                                 <div className="row footer-button">
@@ -114,6 +166,13 @@ class Footer extends Component {
                                 </div>
                             </form>
 
+                        </div>
+                        <div className={`col-xl-8 col-lg-8 thank-message ${this.state.openForm ? "close-thank" : ""}`}>
+                            <div>Thank You for Your order. Please wait for our call!</div>
+                            <div className="return-button">
+
+                                <input className="button" type="submit" value="To home page" onClick={event => this.handleSubmit(event)}></input>
+                            </div>
                         </div>
                     </div>
                     <div className="row">
@@ -133,5 +192,6 @@ class Footer extends Component {
         );
     }
 }
+
 
 export default Footer;
